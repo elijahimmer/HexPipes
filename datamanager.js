2ac5986fe7cfafe4bd20b846fd61d780f08517fe
Elijah M. Immer <me@eimmer.me>
2026-02-18 17:23:45 -0800

    Fix up colors so they are a little nicer.

--- datamanager.js
+++ datamanager.js
@@ -35,7 +35,7 @@ class DataManager {
             /* width */ PARAMETERS.graphWidth,
             /* height */ PARAMETERS.graphHeight,
             /* data */ [this.population, this.uniqueOrganisms],
-            /* label */ "Population (population green, unique red)",
+            /* label */ "Population (population red, unique orange)",
             /* min */ 0, /* no minimum */
             /* max */ 0, /* no maximum */
         );
@@ -48,7 +48,7 @@ class DataManager {
                 /*  width */ half_width,
                 /* height */ PARAMETERS.graphHeight,
                 /*   data */ [this.deathsStarvation, this.deathsRandom],
-                /*  label */ "Cause of Death (starvation green, random red)",
+                /*  label */ "Cause of Death (starvation red, random orange)",
                 /*    min */ 0, /* no minimum */
                 /*    max */ 0, /* no maximum */
             );
@@ -59,7 +59,7 @@ class DataManager {
                 /*  width */ half_width,
                 /* height */ PARAMETERS.graphHeight,
                 /*   data */ [this.energyLostFromDeath, this.pipeFlowLoss],
-                /*  label */ "Energy loss (death green, flow tax red)",
+                /*  label */ "Energy loss (death red, flow tax orange)",
                 /*    min */ 0, /* no minimum */
                 /*    max */ 0, /* no maximum */
             );
@@ -73,7 +73,7 @@ class DataManager {
                 /*  width */ half_width,
                 /* height */ PARAMETERS.graphHeight,
                 /*   data */ [this.pipeChainLengthsAverage, this.pipeChainLengthsLongest],
-                /*  label */ "Chain Lenghts (average green, longest red)",
+                /*  label */ "Chain Lenghts (average red, longest orange)",
                 /*    min */ 0, /* no minimum */
                 /*    max */ 0, /* no maximum */
                 /* resize */ true,
--- util.js
+++ util.js
@@ -1,11 +1,11 @@
 //GameBoard code below
 function randomInt(n) {
     return Math.floor(arng.double() * n);
-};
+}
 
 function distance(p1, p2) {
     return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
-};
+}
 
 function generateNormalSample(mean = 0, stdDev = 1) {
     // box-muller transform
@@ -14,15 +14,15 @@ function generateNormalSample(mean = 0, stdDev = 1) {
 
     let z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(2.0 * Math.PI * u2);
     return z0 * stdDev + mean;
-};
+}
 
 function rgb(r, g, b) {
     return `rgb(${r},${g},${b})`;
-};
+}
 
 function hsl(h, s, l) {
     return `hsl(${h},${s}%,${l}%)`;
-};
+}
 
 function parseHexColor(hex) {
     assert(hex.length == 7, `invalid hex color "${hex}" too short`)
@@ -33,27 +33,33 @@ function parseHexColor(hex) {
         G: parseInt(hex.slice(3,5), 16),
         B: parseInt(hex.slice(5,7), 16),
     };
-};
+}
+
+function standardizeColor(str) {
+    var ctx = document.createElement("canvas").getContext("2d");
+    ctx.fillStyle = str;
+    return ctx.fillStyle;
+}
 
 function download(filename, text) {
     var pom = document.createElement('a');
     pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     pom.setAttribute('download', filename);
     pom.click();
-};
+}
 
 
 function databaseConnected() {
     const dbDiv = document.getElementById("db");
     dbDiv.classList.remove("db-disconnected");
     dbDiv.classList.add("db-connected");
-};
+}
 
 function databaseDisconnected() {
     const dbDiv = document.getElementById("db");
     dbDiv.classList.remove("db-connected");
     dbDiv.classList.add("db-disconnected");
-};
+}
 
 function assert(ok, msg) {
     if (!ok) console.error(msg);
@@ -73,22 +79,15 @@ const base5Order = [
     ["0B3R-1B4R-2B5R"]
 ];
 
+function base15ColorFromIndex(i) {return `hsl(${i * 255.0 / 30.0}, 100%, 60.8%)`;}
+
 // buckets in order
-const base5Colors = ["#00BB00", "#BB0000", "#00BBBB", "#F6C177", "#CCCCCC"];
-const base5ColorsRgb = base5Colors.map((color) => parseHexColor(color));
+const base5Colors = [...[0, 5, 13, 20].map((index) => base15ColorFromIndex(index)), "#FFFFFF"];
+const base5ColorsRgb = base5Colors.map((color) => parseHexColor(standardizeColor(color)));
 
 // should have the same structure as `base5order`
-const base15Colors = [
-    // 3 short
-    "#00BB00", "#00BB5E",
-    // 2 long, 1 short
-    "#BB0000", "#BB5E00", "#BB005E", "#BEBE00", "#BB5E5E", "#BB3333",
-    // short straight
-    "#005EBB","#00BBBB", "#00BB5E",
-    // long straight
-    "#F6C177", "#ECF677", "#EFE810",
-    // straight
-    "#CCCCCC"
-];
-const base15ColorsRgb = base15Colors.map((color) => parseHexColor(color));
+
+const base15Colors = [...[0,2, 6,7,8,9,10,11, 16,18,20, 23,25,27].map((index) => base15ColorFromIndex(index)), "#FFFFFF"];
+
+const base15ColorsRgb = base15Colors.map((color) => parseHexColor(standardizeColor(color)));
 
