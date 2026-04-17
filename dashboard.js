e657079bc49f5f0635e82cbab0fcd84b76970a59
Elijah Immer <me@eimmer.me>
2026-04-17 09:51:29 -0700

    Update dashboard for stats

--- dashboard.js
+++ dashboard.js
@@ -9,7 +9,13 @@ window.requestAnimFrame = (function () {
             };
 })();
 
-var canvas;
+window.canvas = null;
+window.data = [];
+window.data_idx = 0;
+
+const page_limit = 100;
+const last_tick = 200_000;
+window.page = 0;
 
 PARAMETERS.graphWidth = 1200;
 PARAMETERS.canvasWidth = PARAMETERS.graphWidth + PARAMETERS.graphHoriPadding * 4;
@@ -22,7 +28,6 @@ function collectionName() {
     return entry.value || PARAMETERS.collection
 }
 
-window.page = 0;
 var socket = io.connect(PARAMETERS.ip);
 
 socket.on("connect", function () {
@@ -64,28 +69,33 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
         console.log(`query: ${query} filter: ${filter}`);
 
-        socket.emit("count",
-            {
-                db: PARAMETERS.db,
-                collection: collectionName(),
-                query: { "name": query },
-            });
+        socket.emit("count", {
+            db: PARAMETERS.db,
+            collection: collectionName(),
+            query: {
+                "name": query,
+                "last_tick": last_tick
+            },
+        });
+
+    }, false);
+
+    document.getElementById("Prev Query").addEventListener("click", function (e) {
+        if (data_idx > 0) {
+            data_idx -= 1;
+            window.data_manager.loadData(data[data_idx]);
+            document.getElementById("query_info").innerHTML = `${data_idx}/${data.length}`;
+        }
 
     }, false);
 
     document.getElementById("Next Query").addEventListener("click", function (e) {
-        let selector = document.getElementById("run_selection");
-        selector.selectedIndex = (selector.selectedIndex + 1) % selector.options.length;
-        query = selector.value;
-        document.getElementById("query_info").innerHTML = "Query Sent. Awaiting Reply.";
-        filter = null;
+        if (data_idx < data.length) {
+            data_idx += 1;
+            window.data_manager.loadData(data[data_idx]);
+            document.getElementById("query_info").innerHTML = `${data_idx}/${data.length}`;
+        }
 
-        socket.emit("count",
-            {
-                db: PARAMETERS.db,
-                collection: collectionName(),
-                query: { "name": query },
-            });
     }, false);
 
     document.getElementById("download").addEventListener("click", function (e) {
@@ -99,42 +109,43 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 socket.on("count", function (length) {
     numRecords = length;
-    document.getElementById("query_info").innerHTML = `Received ${page + 1} of ${numRecords} records.`;
-    data = [];
-    // for (var i = 0; i < length/page_limit; i++) {
-    socket.emit("find",
-        {
-            db: PARAMETERS.db,
-            collection: collectionName(),
-            query: { "name": query },
-            filter: filter,
-            limit: 1,
-            page: page
-        });
-    // }
+    window.data = [];
+    socket.emit("find", {
+        db: PARAMETERS.db,
+        collection: collectionName(),
+        query: {
+            name: query,
+            last_tick: last_tick
+         },
+        filter: filter,
+        page_limit: page_limit,
+    });
 });
 
 socket.on("find", function (array) {
+    window.data.push(...array);
+    window.page += 1;
+
     if (array.length > 0) {
-        const run = array[0];
-        console.log(`Find: run #${page + 1}`, run)
-
-        document.getElementById("query_info").innerHTML =
-            `Received run #${page + 1}/${numRecords}.`;
-
-
-        if(data.length < numRecords) socket.emit("find",
-            {
+        if (data.length < numRecords) {
+            socket.emit("find", {
                 db: PARAMETERS.db,
                 collection: collectionName(),
-                query: { parameters: {name: query} },
-                filter: filter,
-                limit: 1,
-                page: page++
+                query: {
+                    "name": query,
+                    "last_tick": last_tick
+                 },
+                filter: window.filter,
+                limit: window.page_limit,
+                page: window.page,
             });
+        }
 
-        window.data_manager.loadData(run);
-    } else console.log("Empty data.");
+        document.getElementById("query_info").innerHTML = `${data_idx}/${data.length}`;
+
+        window.data_manager.loadData(data[data_idx]);
+        getStats();
+    }
 });
 
 socket.on("distinct", function (array) {
@@ -157,5 +168,46 @@ function populateDropDown(labels) {
         option.textContent = label;
         runSelect.appendChild(option);
     });
+}
+
+function getStats() {
+    console.log("Stats!");
+
+    const entry_count = 10_000;
+    const success_ratios = [.99, .95, .90, .75, 0];
+
+    let success_counts_base_5 = [0, 0, 0, 0, 0];
+    let success_ratio_base_5 = 0.0;
+
+    let success_counts_base_15 = [0, 0, 0, 0, 0];
+
+    data.forEach((entry) => {
+        const radius = entry.params.gridRadius;
+        const cell_count = 3 * (radius * radius) - 3 * radius - 1;
+
+        { // base 5 successes
+            const dominant_species_count = entry.base5Pops.reduce((acc, cur) => {
+                const cur_count = cur.slice(-entry_count).reduce((sum, val) => sum + val, 0);
+
+                if (cur_count > acc) return cur_count
+                return acc
+            }, 0)
+
+            const success_ratio = dominant_species_count / (cell_count * entry_count);
+            success_ratio_base_5 += success_ratio;
+
+            let counts = success_ratios.map((ratio) => success_ratio >= ratio);
+
+            for (const [idx, element] of success_counts_base_5.entries()) {
+                success_counts_base_5[idx] += counts[idx];
+            }
+        }
+    });
+
+    const stats = document.getElementById("stats");
+    stats.textContent = `
+        successes base 5: ${success_counts_base_5}
+        average success ratio base 5: ${success_ratio_base_5 / data.length}
+    `
 }
 
