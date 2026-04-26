ca311b720b5dc840a6df89798dc2b903c635e498
Elijah Immer <me@eimmer.me>
2026-04-25 20:21:58 -0700

    Increase reporting period--we don't need that much

--- params.js
+++ params.js
@@ -4,7 +4,7 @@ const DEFAULT_PARAMETERS = {
     // Framework parameters
     updatesPerTick: 1,
     ticksPerDraw: 1000,
-    reportingPeriod: 100,
+    reportingPeriod: 200,
     db: "HexPipes",
     collection: "normal-data-start-2026-04-22",
     ip: "https://73.19.38.112:8888", // Canvas parameters
